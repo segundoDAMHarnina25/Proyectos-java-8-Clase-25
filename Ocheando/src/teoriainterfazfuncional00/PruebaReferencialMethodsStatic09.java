44ca09fe833692799b9412574c5dd8a0a06d040f
programacion <damharnina25@iesarroyoharnina.es>
2024-09-20 09:08:02 +0200

    olvido

--- Ocheando/src/teoriainterfazfuncional00/PruebaReferencialMethodsStatic09.java
+++ Ocheando/src/teoriainterfazfuncional00/PruebaReferencialMethodsStatic09.java
@@ -3,11 +3,20 @@ package teoriainterfazfuncional00;
 public class PruebaReferencialMethodsStatic09 {
 
 	public static void main(String[] args) {
-		Converter<Float,String> converterLambda=(String f) -> {return Float.valueOf(f);};
+		Converter<Float,String> converterLambda=(String f) 
+				-> {return Float.valueOf(f);};
 		Converter<Float,String> converter=
 				Float::valueOf;
 		Float otro=converter.convert("16.9");
 		System.out.println(otro);
 	}
 
+}
+class ParaConverter{
+	public static Float convertir(String valor) {
+		if(valor.startsWith("a")) {
+			return 5f;
+		}
+		return -1f;
+	}
 }
